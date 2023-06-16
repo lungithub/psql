1faf2033a8f171478031fb9e8c3b482eecacf960
afoot <afoot@homeoffce.wal-mart.com>
2023-06-15 21:38:22 -0700

    updates

--- install/psql_copy_sudo.sh
+++ install/psql_copy_sudo.sh
@@ -6,14 +6,12 @@
 #
 # Perms for refernce
 #
-#ls -l /etc/group
-#-rw-r--r-- 1 root root 373 Jan  9 01:53 /etc/group
-#
-#-> ls -l /etc/group
-#-rw-r--r-- 1 root root 373 Jan  9 01:53 /etc/group
+# ls -l /etc/group
+# -rw-r--r-- 1 root root 373 Jan  9 01:53 /etc/group
 #
 
-BASEDIR=/hostdata/data/env_config/psql_config
+BASEDIR=/hostdata/app/psql
+USER=postgres
 
 # check perms before and after copy
 cp /etc/sudoers /etc/sudoers.$$
@@ -26,13 +24,5 @@ ls -l /etc/sudoers
 grep postgres /etc/sudoers || echo "postgres not in /etc/sudoers"
 echo 
 
-# check perms before and after copy
-cp /etc/group /etc/group.$$
-cp ${BASEDIR}/files/etc_group /etc/group
-chmod 644 /etc/group
-echo
-echo "Verify postgres in the GROUPS file"
-echo
-ls -l /etc/group
-grep postgres /etc/group || echo "postgres not in /etc/group"
-echo  
+# add the user to the wheel sudo group
+usermod -a -G wheel $USER
